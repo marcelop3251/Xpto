35d749035b49aaee1e26179f2b2eb595ccd7ed7e
marcelo.palma <marcelop3251@gmail.com>
2018-02-03 12:46:49 -0200

    Adicionado respostas Http

--- src/main/java/br/com/xpto/restcontroller/CidadeRest.java
+++ src/main/java/br/com/xpto/restcontroller/CidadeRest.java
@@ -2,10 +2,12 @@ package br.com.xpto.restcontroller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.xpto.entidades.Capital;
 import br.com.xpto.entidades.Cidade;
@@ -49,12 +51,14 @@ public class CidadeRest {
 	
 	
 	@RequestMapping(value="adicionarCidade", method = RequestMethod.POST)
+	@ResponseStatus(value = HttpStatus.CREATED)
 	public Cidade addCidade(@RequestBody Cidade cidade){
 		return  cidadeService.adicionarCidade(cidade);
 		
 	}
 	
 	@RequestMapping(value="cidade/{idIbge}", method = RequestMethod.DELETE)
+	@ResponseStatus(value = HttpStatus.OK)
 	public void deleteCidade(@PathVariable(value = "idIbge") Integer idIbge){
 		cidadeService.deleteCidade(idIbge);
 	}
